d9c2d5ec5140a89464131724f4ea294f5d3ba4de
KaneGreen <737445366KG@Gmail.com>
2020-12-07 09:16:37 +0800

    remove merged SSRP PRs

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -167,8 +167,6 @@ sed -i '/V2ray:v2ray/d' package/new/luci-app-passwall/Makefile
 # SSRP modification
 pushd package/lean
 wget -qO - https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/244.patch | patch -p1
-wget -qO - https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/250.patch | patch -p1
-wget -qO - https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/253.patch | patch -p1
 popd
 sed -i 's,default n,default y,g' package/lean/luci-app-ssr-plus/Makefile
 sed -i '/V2ray:v2ray/d' package/lean/luci-app-ssr-plus/Makefile
