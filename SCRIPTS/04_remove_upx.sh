ef3abbaf818922615d08efb83c24ddc8ce5a0f03
KaneGreen <737445366KG@Gmail.com>
2021-02-20 11:06:54 +0800

    beautify 04_remove_upx.sh

--- SCRIPTS/04_remove_upx.sh
+++ SCRIPTS/04_remove_upx.sh
@@ -3,9 +3,9 @@
 # Use it under GPLv3, please.
 # --------------------------------------------------------
 # Remove upx commands
-makefile_file="$({ find package|grep Makefile |sed "/Makefile./d"; } 2>"/dev/null")"
-for a in ${makefile_file}
+MAKEFILE_FILES=$({ find package | grep 'Makefile' | sed '/Makefile./d' ; } 2>/dev/null)
+for a in ${MAKEFILE_FILES}
 do
-	[ -n "$(grep "upx" "$a")" ] && sed -i "/upx/d" "$a"
+	grep -q 'upx' "$a" 2>/dev/null && sed -i '/upx/d' "$a"
 done
 exit 0
