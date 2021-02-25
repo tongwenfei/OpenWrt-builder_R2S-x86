2a16ae1cca83b4442e4823f8fab50d707a793a3d
KaneGreen <737445366KG@Gmail.com>
2021-02-25 17:35:35 +0800

    Revert "fix ssr-libev build error on GCC 10" because upstream merged
    This reverts commit 9c0b0e3388a9920ce1b5654ed27fa8377baa8f48.

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -144,8 +144,6 @@ svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus          
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks                      package/new/ssocks
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin                package/new/v2ray-plugin
 svn co https://github.com/fw876/helloworld/trunk/xray-core                             package/new/xray-core
-# shadowsocksr-libev的GCC 10编译错误修复
-wget -qO - https://patch-diff.githubusercontent.com/raw/coolsnowwolf/lede/pull/6433.patch | patch -p1
 # 订阅转换
 svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/ctcgfw/subconverter package/new/subconverter
 svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/ctcgfw/jpcre2       package/new/jpcre2
