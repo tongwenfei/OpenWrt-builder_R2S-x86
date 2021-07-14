f63890bb26eee9b964487e6c9e1351fc6b12a6c1
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-07-14 13:27:50 +0800

    Update 02_target_only.sh

--- SCRIPTS/R4S/02_target_only.sh
+++ SCRIPTS/R4S/02_target_only.sh
@@ -62,6 +62,15 @@ CONFIG_CRYPTO_NHPOLY1305_NEON=y
 CONFIG_CRYPTO_AES_ARM64_BS=y
 ' >> ./target/linux/rockchip/armv8/config-5.4
 
+# MPTCP
+echo '
+CONFIG_MPTCP=y
+CONFIG_MPTCP_PM_ADVANCED=y
+CONFIG_MPTCP_FULLMESH=y
+CONFIG_DEFAULT_FULLMESH=y
+CONFIG_DEFAULT_MPTCP_PM="fullmesh"
+' >> ./target/linux/rockchip/armv8/config-5.4
+
 #Vermagic
 latest_version="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" |sed -n '/21/p' |sed -n 1p |sed 's/v//g' |sed 's/.tar.gz//g')"
 wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/armv8/packages/Packages.gz
