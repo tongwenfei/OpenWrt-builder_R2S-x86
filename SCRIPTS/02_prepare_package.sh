941923be7bf2403a296bf15470d5f6eb363bd293
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-07-15 15:30:14 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -35,6 +35,9 @@ wget -P target/linux/generic/hack-5.4/ https://github.com/Ysurac/openmptcprouter
 wget -qO - https://github.com/Ysurac/openmptcprouter/raw/develop/patches/nanqinlang.patch | patch -p1
 wget -qO - https://github.com/Ysurac/openmptcprouter/raw/develop/patches/bbr2.patch | patch -p1
 
+# OPENSSL
+wget -qO - https://github.com/mj22226/openwrt/commit/5e1063.patch | patch -p1
+
 ### 必要的 Patches ###
 # Patch arm64 型号名称
 wget -P target/linux/generic/hack-5.4/ https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/generic/hack-5.4/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch
