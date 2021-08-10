27d1ac6bf6ff5e1d74e7ba6b536fd6f4cf170499
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-08-10 16:59:33 +0800

    Add CacULE Scheduler Support

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -35,6 +35,9 @@ cp -f ../PATCH/dnsmasq/900-add-filter-aaaa-option.patch ./package/network/servic
 patch -p1 < ../PATCH/BBRv2/openwrt-kmod-bbr2.patch
 cp -f ../PATCH/BBRv2/693-Add_BBRv2_congestion_control_for_Linux_TCP.patch ./target/linux/generic/hack-5.4/693-Add_BBRv2_congestion_control_for_Linux_TCP.patch
 wget -qO - https://github.com/openwrt/openwrt/commit/cfaf039.patch | patch -p1
+# CacULE
+wget -qO - https://github.com/QiuSimons/openwrt-NoTengoBattery/commit/336a9b8.patch | patch -p1
+wget -P target/linux/generic/hack-5.4/ https://github.com/hamadmarri/cacule-cpu-scheduler/raw/master/patches/CacULE/v5.4/cacule-5.4.patch
 # Grub 2
 wget -qO - https://github.com/QiuSimons/openwrt-NoTengoBattery/commit/afed16a.patch | patch -p1
 # Haproxy
