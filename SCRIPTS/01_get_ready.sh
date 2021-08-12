7372899e2c3a3fc014496845a440f4eb23edd20f
KaneGreen <737445366KG@Gmail.com>
2021-08-12 16:32:09 +0800

    build 03

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -3,6 +3,6 @@ set -e
 # use openwrt-21.02 branch
 git clone --single-branch -b openwrt-21.02 https://git.openwrt.org/openwrt/openwrt.git openwrt
 # download others' source for some packages
-git clone -b main --depth=1 https://github.com/Lienol/openwrt.git          openwrt-lienol
+git clone -b main --depth=1 https://github.com/Lienol/openwrt.git openwrt-lienol
 rm -rf ./*-lienol/.git
 exit 0
