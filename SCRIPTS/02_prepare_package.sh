e9c1632ceca57a927102b5b4e422b05076b61ef0
KaneGreen <737445366KG@Gmail.com>
2021-09-03 10:19:42 +0800

    update naiveproxy

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -156,6 +156,7 @@ git clone -b dev --depth=1 https://github.com/vernesong/OpenClash               
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus                      package/lean/luci-app-ssr-plus
 pushd package/lean
   patch -p1 < ../../../PATCH/0005-add-QiuSimons-Chnroute-to-chnroute-url.patch
+  wget -qO- https://patch-diff.githubusercontent.com/raw/fw876/helloworld/pull/639.patch | patch -p1
 popd
 # 订阅转换
 svn co https://github.com/immortalwrt/packages/trunk/libs/jpcre2      feeds/packages/libs/jpcre2
