43052414ed5db19e8e1ddbfad32bec7aaca33768
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-09-23 20:13:42 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -282,6 +282,7 @@ rm -rf ./package/lean/luci-app-ssr-plus/po/zh_Hans
 pushd package/lean
 wget -qO - https://github.com/fw876/helloworld/pull/645.patch | patch -p1
 wget -qO - https://github.com/fw876/helloworld/pull/656.patch | patch -p1
+wget -qO - https://github.com/fw876/helloworld/pull/664.patch | patch -p1
 wget -qO - https://github.com/QiuSimons/helloworld-fw876/commit/5bbf6e7.patch | patch -p1
 wget -qO - https://github.com/QiuSimons/helloworld-fw876/commit/323fbf0.patch | patch -p1
 popd
